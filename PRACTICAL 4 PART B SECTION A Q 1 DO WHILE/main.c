03f49cf84d28b2853f28dc747c4e9056abceedff
mnpjayaweera <136550596+mnpjayaweera@users.noreply.github.com>
2023-07-30 22:39:02 +0530

    Add files via upload

---
+++ PRACTICAL 4 PART B SECTION A Q 1 DO WHILE/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int i=0;
+  do
+  {
+     printf("%d  ",i);
+     i++;
+  }
+  while (i<=100);
+
+}
---
+++ PRACTICAL 4 PART B SECTION A Q 3/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=0,num;
+    printf("ENTER A NUMBER:");
+    scanf("%d",&num);
+    while (i<=num)
+        printf("");
+    i++;
+}
